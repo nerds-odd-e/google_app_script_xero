59c850426f28594043c466c0a0966d9ba4de2e2c
kinwah <kinwah.lai@odd-e.com>
2014-02-11 16:53:00 +0800

    populate spreadsheet header

--- xero_integration.js
+++ xero_integration.js
@@ -20,16 +20,9 @@ var oauthOptions = {
 function main() {
   if (consumerKey && consumerSecret && spreadSheetId) {
     fetchingInvoices();
+  } else {
+    Logger.log('Please set xeroConsumerKey, xeroConsumerSecret and spreadSheetId');
   }
-}
-
-function fetchingOrgInfo() {
-  Logger.log('Access organisation starts');
-  var url = 'https://api.xero.com/api.xro/2.0/Organisation';
-  var response = UrlFetchApp.fetch(url, oauthOptions).getContentText();
-  var document = XmlService.parse(response);
-  var xml = XmlService.getPrettyFormat().format(document);
-  Logger.log('Ended with response \n' + xml);
 }
 
 function fetchingInvoices() {
@@ -37,13 +30,29 @@ function fetchingInvoices() {
   var url = 'https://api.xero.com/api.xro/2.0/Invoices';
   var response = UrlFetchApp.fetch(url, oauthOptions).getContentText();
   var document = XmlService.parse(response);
-  var xml = XmlService.getPrettyFormat().format(document);
-  Logger.log('Ended with response \n' + xml);
+  Logger.log('Access invoices ends');
+  populateSpreadsheetWithInvoices(document);
 }
 
-function populateSpreadsheetWithInvoices(xml) {
-  SpreadsheetApp.ope
+function populateSpreadsheetWithInvoices(xmlDocument) {
   var spreadsheet = SpreadsheetApp.openById(spreadSheetId);
   var sheet1 = spreadsheet.getSheets()[0];
-  sheet1.getRange(1, 1, 2, 2).setValues([ [ 'One', 'Two' ], [ 1, 2 ] ]);
+
+  var root = xmlDocument.getRootElement();
+  var docId = root.getChildText('Id'); 
+  var resStatus = root.getChildText('Status');
+  if (resStatus === 'OK') {
+    Logger.log('Doc Id ' + docId);
+    var invoices = root.getChild('Invoices').getChildren();
+    // create spreadsheet header
+    populateSpreadsheetHeader(sheet1, invoices[0].getChildren());
+  }
+}
+
+function populateSpreadsheetHeader(sheet, headers) {
+  var column = []; 
+  for (var i = 0; i < headers.length; i++) {
+    column.push(headers[i].getName());
+  }
+  sheet.getRange(1, 1, 1, headers.length).setValues([ column ]);
 }
