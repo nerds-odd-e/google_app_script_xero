905a9c2c08a572323a67e0fc0c2a7b591eca253c
kinwah <kinwah.lai@odd-e.com>
2014-02-13 13:29:14 +0800

    construct the xml from spreadsheet and post it to xero

--- importToXero.js
+++ importToXero.js
@@ -1,53 +1,85 @@
-var consumerKey = UserProperties.getProperty('xeroConsumerKey'); 
-var consumerSecret = UserProperties.getProperty('xeroConsumerSecret'); 
-
-var oauthConfig = UrlFetchApp.addOAuthService('xero');
-oauthConfig.setAccessTokenUrl(
-    'https://api.xero.com/oauth/AccessToken');
-oauthConfig.setRequestTokenUrl(
-    'https://api.xero.com/oauth/RequestToken');
-oauthConfig.setAuthorizationUrl(
-    'https://api.xero.com/oauth/Authorize');
-oauthConfig.setConsumerKey(consumerKey);
-oauthConfig.setConsumerSecret(consumerSecret);
-
-var oauthOptions = {
-  'oAuthServiceName' : 'xero',
-  'oAuthUseToken' : 'always'
-};
-
 var spreadSheetId = UserProperties.getProperty('importSpreadSheetId');
 function main() {
-  importInvoiceFromSpreadsheet();
+  if (XeroOAuth.setup() && spreadSheetId) {
+    importInvoiceFromSpreadsheet();
+  } else {
+    Logger.log('Please run the XeroOAuth.setup()');
+  }
 }
 
 function importInvoiceFromSpreadsheet() {
-//  var spreadsheet = SpreadsheetApp.openById(spreadSheetId);
-//  var sheet1 = spreadsheet.getSheets()[0];
-  var data = '<Invoices> \
-  <Invoice> \
-    <Type>ACCREC</Type> \
-    <Contact> \
-      <Name>Lai Kin Wah</Name> \
-    </Contact> \
-    <Date>2014-02-12T00:00:00</Date> \
-    <DueDate>2014-02-19T00:00:00</DueDate> \
-    <LineAmountTypes>Exclusive</LineAmountTypes> \
-    <LineItems> \
-      <LineItem> \
-        <Description>Monthly rental for property at 56a Wilkins Avenue</Description> \
-        <Quantity>4.3400</Quantity> \
-        <UnitAmount>395.00</UnitAmount> \
-        <AccountCode>200</AccountCode> \
-      </LineItem> \
-    </LineItems> \
-  </Invoice> \
-</Invoices>'
-  var urlOptions = oauthOptions;
+  var spreadsheet = SpreadsheetApp.openById(spreadSheetId);
+  var sheet1 = spreadsheet.getSheets()[0];
+  // get the 1st row (frozen) , the header
+  var dataRange = sheet1.getRange(1, 1, sheet1.getLastRow(), sheet1.getLastColumn());
+  var dataObjects = getRowsData(sheet1, dataRange, 1);
+  
+  var invoices = XmlService.createElement('Invoices');
+
+  for(var i = 0; i < dataObjects.length; i++) {
+    // skip 1st data row because it is a header row
+    if(i === 0 || dataObjects[i].invoiceNumber === null || dataObjects[i].invoiceNumber === undefined) {
+      continue;
+    }
+    var invoice = XmlService.createElement('Invoice');
+    invoice.addContent(XmlService.createElement('Type').setText('ACCREC'));
+    invoice.addContent(XmlService.createElement('InvoiceNumber').setText(dataObjects[i].invoiceNumber));
+    invoice.addContent(XmlService.createElement('LineAmountTypes').setText('NoTax'));
+    invoice.addContent(XmlService.createElement('CurrencyCode').setText(dataObjects[i].currency)); 
+    invoice.addContent(XmlService.createElement('Status').setText('DRAFT'));
+    invoice.addContent(XmlService.createElement('Date').setText(dataObjects[i].date.replace(/\./g,'-')));
+    
+    createContact(dataObjects[i].to, invoice);
+    
+    createLineItems(dataObjects[i], invoice);
+    
+    invoices.addContent(invoice);
+  };
+
+  var xmlDocument = XmlService.createDocument(invoices);
+  
+  var urlOptions = XeroOAuth.oauthOptions;
   urlOptions.method = 'post';
-  urlOptions.payload = data;
+  urlOptions.payload = XmlService.getRawFormat().format(xmlDocument);
   urlOptions.contentType = 'application/xml; charset=utf-8';
   var url = 'https://api.xero.com/api.xro/2.0/Invoices';
-  var response = UrlFetchApp.fetch(url, oauthOptions).getContentText();
+  var response = UrlFetchApp.fetch(url, urlOptions).getContentText();
   Logger.log(response);
+}
+
+function createContact(contactData, invoice) {
+  var lines = contactData.split('\n');
+  
+  if(lines === null || lines === undefined || lines.length === 0) {
+    return;
+  }
+  var contact = XmlService.createElement('Contact');
+  contact.addContent(XmlService.createElement('Name').setText(lines[0]));
+  
+  invoice.addContent(contact);
+  return;
+}
+
+function createLineItems(dataObject, invoice) {
+  if((dataObject.entry1 === null || dataObject.entry1 === undefined) && 
+     (dataObject.entry2 === null || dataObject.entry2 === undefined) && 
+     (dataObject.entry3 === null || dataObject.entry3 === undefined) &&
+     (dataObject.entry4 === null || dataObject.entry4 === undefined)) {
+       return;
+     }
+  var lineItems = XmlService.createElement('LineItems');
+  for(var i = 1; i < 5; i++) {
+    var entryKey = 'entry' + i;
+    var amountKey = 'amount' + i;
+    if(dataObject[entryKey] !== null && dataObject[entryKey] !== undefined){
+      var lineItem = XmlService.createElement('LineItem');
+      lineItem.addContent(XmlService.createElement('Description').setText(dataObject[entryKey]));
+      lineItem.addContent(XmlService.createElement('Quantity').setText(1));
+      lineItem.addContent(XmlService.createElement('UnitAmount').setText(dataObject[amountKey]));
+      lineItem.addContent(XmlService.createElement('AccountCode').setText(200));
+      lineItems.addContent(lineItem);
+    }
+  }
+  invoice.addContent(lineItems);
+  return; 
 }
